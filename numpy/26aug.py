7c170bf09d3ba5491c32e50b2b93fa90013e37d5
dishant1123 <dishantshah1417@gmail.com>
2026-08-26 19:07:59 +0530

    ....

---
+++ numpy/26aug.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+# data = np.genfromtxt("numpy_dataset.csv",delimiter=",",dtype=float,skip_header=1)
+# print(data)
+
+# base_price = data[:,2]
+# month_1_unit = data[:,3]
+# revenue =base_price*month_1_unit
+
+# print("month_1_units : ",revenue) 
+
+# total  revenue  month_1 unit  ------>650500
+# total  revenue  month_2 unit  ------>749000
+# total  revenue  month_3 unit  ------>853500
+
+# total revenue  for all  product = 2253000 
+# total  profit =  
+
+"""
+profit = revenue - cost
+
+total cost = 
+"""

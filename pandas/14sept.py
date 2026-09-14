dfb81dbd06d472ba491cb91d7355a5bd1c70405e
dishant1123 <dishantshah1417@gmail.com>
2026-09-14 16:51:56 +0530

    .....

--- pandas/14sept.py
+++ pandas/14sept.py
@@ -19,14 +19,14 @@
 import  pandas as pd
 import numpy as np
 
-df =pd.DataFrame({
+"""df =pd.DataFrame({
     "id" :[1,2,3,4,5,6,7,8,9,10],
     'name' : [np.nan,"sita","ravan","bhudev","sahdev","minaxi","yug","ravan","bhudev","sahdev"],
     "age" :[56,25,44,41,np.nan,23,27,56,25,np.nan],
     "salary":[10000,20000,np.nan,8580,np.nan,96000,np.nan,10000,20000,np.nan]
 })
 print(df)
-
+"""
 """
 task  :1 person  who has salary  more than 20000 -----> name  , salary  ,age 
 task : 2  print  col wise  missing value count
@@ -37,3 +37,40 @@ task : 6 assign the  new  index number and  sort  this  ----> using sort_index
 task : 7 reset_index number  for  above task 5.    
 """
 
+"""
+days     sales 
+1         1000
+2         1500
+3         1700
+4         1300
+5         1200  
+6         2000  
+7         1800
+8         2300  
+9         2500
+10        50000  -----> 6000
+
+outlier ----> its  effected to my avg  sales.
+
+two ways  : 
+1. IQR method  ----> inter quartile range
+
+IQR = Q3 - Q1 -----> Q3 = 75 th percentile , Q1 = 25 th percentile
+
+q1 = np.quantile(df['sales'],0.25)
+q3= np.quantile(df['sales'],0.75)
+
+IQR = q3 - q1
+
+lower_limit = q1 - 1.5 * IQR
+upper_limit = q3 + 1.5 * IQR -----> 6000
+
+2. z score method  ----> z score
+from scipy.stats import zscore
+z_score = zscore(df['sales'])
+
+z_score > 2    -----> outlier
+
+# clip : 
+
+"""
